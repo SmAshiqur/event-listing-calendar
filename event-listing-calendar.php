ebf6af5e0609f91467dfae2a3004d5bac99da296
SmAshiqur <rahman_nayeem@live.com>
2025-08-19 17:12:32 +0600

    Test 6

--- event-listing-calendar.php
+++ event-listing-calendar.php
@@ -3,7 +3,7 @@
 Plugin Name: Event Listing Calendar
 Plugin URI: https://masjidsolutions.net/
 Description: A plugin to display events in a calendar and list format using FullCalendar and Flatpickr.
-Version: 3.1
+Version: 3.2
 Author: MASJIDSOLUTIONS
 Author URI: https://masjidsolutions.net/
 License: GPL2
@@ -15,7 +15,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Fixed version constant to match header
-define('ELC_VERSION', '3.1');
+define('ELC_VERSION', '3.2');
 define('ELC_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('ELC_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
